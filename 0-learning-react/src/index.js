5b2a01c3dc43fd1c0b26fa251b864bd60dbe047c
vSpiryakov <vspiryakov@gmail.com>
2023-01-23 19:59:31 +0900

    Continues studying React and experimenting with the first component

--- 0-learning-react/src/index.js
+++ 0-learning-react/src/index.js
@@ -1,8 +1,46 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+// css
+import './index.css';
 
-function Greeting(){
-  return <h4>this is my first component</h4>
+const books = [
+  {
+    author: 'Author Name',
+    title: 'Book Title',
+    img: 'https://m.media-amazon.com/images/I/31-thesnl6S._SS135_.jpg',
+  },
+  {
+    author: 'Author Name 2',
+    title: 'Book Title 2',
+    img: 'https://m.media-amazon.com/images/I/31-thesnl6S._SS135_.jpg',
+  },
+];
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting/>,document.getElementById('root'))
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src={props.img} alt='book'></img>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
