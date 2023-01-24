630cb1fec6818ff40df88d387c44efdb994dc1c9
vSpiryakov <vspiryakov@gmail.com>
2023-01-24 18:06:02 +0900

    All basics are basically covered (the last commit)

--- 0-learning-react/src/index.js
+++ 0-learning-react/src/index.js
@@ -5,40 +5,36 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     author: 'Author Name',
     title: 'Book Title',
     img: 'https://m.media-amazon.com/images/I/31-thesnl6S._SS135_.jpg',
   },
   {
+    id: 2,
     author: 'Author Name 2',
     title: 'Book Title 2',
-    img: 'https://m.media-amazon.com/images/I/31-thesnl6S._SS135_.jpg',
+    img: 'https://m.media-amazon.com/images/I/51DnVjJHoBL._SS135_.jpg',
   },
 ];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
-      <img src={props.img} alt='book'></img>
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt='book'></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
